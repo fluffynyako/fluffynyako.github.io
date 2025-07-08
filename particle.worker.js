de433f9be206045a1336ce4ce5571c0070c3a1d4
fluffynyako 🏳️‍⚧️🇨🇭 <152802829+fluffynyako@users.noreply.github.com>
2025-07-08 15:35:23 +0200

    Add files via upload

--- particle.worker.js
+++ particle.worker.js
@@ -2,41 +2,24 @@ let canvas;
 let ctx;
 let particles = [];
 let stardusts = [];
-let pathQueue = [];
 
 let config = {
     stardustColor: '#ffffff',
     stardustCount: 70,
-};
-
-const pointer = {
-    x: 0,
-    y: 0,
-    isTracking: false,
-};
-
-const emitter = {
-    x: 0,
-    y: 0,
-    prevX: 0,
-    prevY: 0,
-    lerpAmount: 0.2,
-    particleDensity: 3,
+    maxParticles: 250,
 };
 
 class Particle {
-    constructor(x, y, isSpark) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        const angle = Math.random() * Math.PI * 2;
-        const speed = isSpark ? Math.random() * 3 + 1.5 : Math.random() * 1.5;
-        this.speedX = Math.cos(angle) * speed;
-        this.speedY = Math.sin(angle) * speed;
-        this.life = isSpark ? Math.random() * 0.4 + 0.2 : Math.random() * 0.8 + 0.5;
-        this.decay = isSpark ? 0.04 : 0.015;
-        this.size = isSpark ? Math.random() * 2 + 1 : Math.random() * 3.5 + 1;
-        const colors = isSpark ? ['#ffffff', '#ff6b9d'] : ['#a0e7fa', '#f7a8b8'];
+        this.size = Math.random() * 3.5 + 1.5;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1;
+        const colors = ['#a0e7fa', '#f7a8b8', '#ffffff', '#ff6b9d'];
         this.color = colors[Math.floor(Math.random() * colors.length)];
+        this.life = Math.random() * 0.8 + 0.5;
+        this.decay = 0.015;
     }
     update() {
         this.x += this.speedX;
@@ -92,38 +75,6 @@ function animate() {
         star.update();
         star.draw(ctx);
     });
-
-    if (pointer.isTracking && pathQueue.length === 0) {
-        pathQueue.push({ x: pointer.x, y: pointer.y });
-    }
-
-    if (pathQueue.length > 0) {
-        const target = pathQueue[0];
-        const dx = target.x - emitter.x;
-        const dy = target.y - emitter.y;
-        
-        emitter.prevX = emitter.x;
-        emitter.prevY = emitter.y;
-
-        if (Math.hypot(dx, dy) < 1) {
-            emitter.x = target.x;
-            emitter.y = target.y;
-            pathQueue.shift();
-        } else {
-            emitter.x += dx * emitter.lerpAmount;
-            emitter.y += dy * emitter.lerpAmount;
-        }
-
-        const moveDist = Math.hypot(emitter.x - emitter.prevX, emitter.y - emitter.prevY);
-        const steps = Math.max(1, Math.floor(moveDist / emitter.particleDensity));
-        for (let i = 0; i < steps; i++) {
-            if (particles.length >= 250) break;
-            const t = i / steps;
-            const x = emitter.prevX + (emitter.x - emitter.prevX) * t;
-            const y = emitter.prevY + (emitter.y - emitter.prevY) * t;
-            particles.push(new Particle(x, y, false));
-        }
-    }
     
     particles = particles.filter(p => {
         p.update();
@@ -154,27 +105,20 @@ self.onmessage = (e) => {
             config.stardustColor = e.data.stardustColor;
             break;
         case 'pointerStart':
-            pointer.isTracking = true;
-            pointer.x = e.data.x;
-            pointer.y = e.data.y;
-            pathQueue = [{ x: e.data.x, y: e.data.y }];
-            emitter.x = e.data.x;
-            emitter.y = e.data.y;
+            if (particles.length < config.maxParticles) {
+                particles.push(new Particle(e.data.x, e.data.y));
+            }
             break;
         case 'pointerMove':
-            if (pointer.isTracking) {
-                pointer.x = e.data.x;
-                pointer.y = e.data.y;
-                pathQueue.push({ x: e.data.x, y: e.data.y });
-                if (particles.length < 250) {
-                     for (let i = 0; i < 2; i++) {
-                        particles.push(new Particle(e.data.x, e.data.y, true));
-                     }
-                }
+            if (e.data.points) {
+                e.data.points.forEach(point => {
+                    if (particles.length < config.maxParticles) {
+                        particles.push(new Particle(point.x, point.y));
+                    }
+                });
             }
             break;
         case 'pointerEnd':
-            pointer.isTracking = false;
             break;
     }
 };
